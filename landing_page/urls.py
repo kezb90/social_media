f0cdda91d87d993cdee954cd50ec21be13ba19dd
kezb90 <92123322+kezb90@users.noreply.github.com>
2024-02-28 23:42:49 +0330

    add master and main template and css js bootstrap and other static files

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from .views import landing_page, about_us
 
-app_name = "landing-page"
-urlpatterns = []
+app_name = "home"
+urlpatterns = [
+    path('', landing_page, name='main'),
+    path('about_us', about_us, name='about-us'),
+]
--- landing_page/views.py
+++ landing_page/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def landing_page(request):
+    return render(request, 'main.html')
+
+def about_us(request):
+    return render(request, 'about_us.html')
