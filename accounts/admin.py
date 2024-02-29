05e7b93eca217713e206315971756e92e6b9e6eb
kezb90 <92123322+kezb90@users.noreply.github.com>
2024-02-29 18:04:10 +0330

    update admin pannel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from django.contrib.admin import register
+from .models import Profile
 # Register your models here.
+
+
+@register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "age",
+        "birthday",
+        "created_at",
+        'bio',
+    )
+    list_display_links = ("id", "user")
+    search_fields = ("user__fisrt_name", "user__last_name", 'id')
