8d2b8757d47f39516335782ec4c9a82317c380b4
kezb90 <92123322+kezb90@users.noreply.github.com>
2024-03-02 22:54:41 +0330

    create models of content app

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from .models import Post, Image, Video, Audio
 
 # Register your models here.
+
+
+class ImageInline(admin.StackedInline):
+    model = Image
+    extra = 1
+
+class VideoInline(admin.StackedInline):
+    model = Video
+    extra = 1
+
+class AudioInline(admin.StackedInline):
+    model = Audio
+    extra = 1
+    
+@register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "title",
+        "caption",
+        "created_at",
+        "updated_at",
+        'is_active',
+    )
+    list_display_links = ("id", "title")
+    search_fields = ("title", "caption", "id")
+    inlines = [ImageInline, VideoInline, AudioInline]
--- content/models.py
+++ content/models.py
@@ -1,3 +1,54 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from accounts.models import MyBaseModel
 # Create your models here.
+
+
+class Post(MyBaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 200)
+    caption = models.TextField()
+    like = models.ManyToManyField(User, blank=True, related_name='likes')
+    mention = models.ManyToManyField(User, blank=True, related_name='mentions')
+    view = models.ManyToManyField(User, blank=True, related_name='views')
+    
+    
+    def total_like(self):
+        return self.like.count()
+    
+    def total_view(self):
+        return self.view.count()
+    
+    def mentions(self):
+        return self.view.all()
+    
+
+    def __str__(self):
+        return self.title
+    
+class Image(MyBaseModel):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="images")
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="Post/Media/image/")
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+    
+class Video(MyBaseModel):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="videos")
+    title = models.CharField(max_length=255)
+    video_file = models.FileField(upload_to="Post/Media/viedo/")
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Audio(MyBaseModel):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="audios")
+    title = models.CharField(max_length=255)
+    audio_file = models.FileField(upload_to="Post/Media/audio/")
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
