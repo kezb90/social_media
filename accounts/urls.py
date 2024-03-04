85c24c150e5cc332ebbe8753ccf6bf5d46547a79
kezb90 <92123322+kezb90@users.noreply.github.com>
2024-03-04 19:54:36 +0330

    update urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
     path('profile/update/', ProfileUpdateView.as_view(), name='profile-update'),
-    path('profile/<int:pk>/', PublicProfileView.as_view(), name='public-profile'),
+    path('profile/<int:pk>/', PublicProfileView.as_view(), name='see-other-profile'),
     path('follower-following/', FollowerFollowingView.as_view(), name='follower-following'),
-    path('public-profiles/', PublicProfileListView.as_view(), name='public-profiles'),
+    path('public-profiles/', PublicProfileListView.as_view(), name='public-profiles-list'),
     path('follow/', FollowActionView.as_view(), name='follow-action'),
     path('unfollow/', UnfollowActionView.as_view(), name='unfollow-action'),
 
