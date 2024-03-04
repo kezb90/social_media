e95c13e967056531fd038fb3cd61bc3103a0b622
kezb90 <92123322+kezb90@users.noreply.github.com>
2024-03-04 19:36:44 +0330

    admin panel admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,18 +4,48 @@ from .models import Profile, Follow
 
 # Register your models here.
 
+# Inline class for followers
+class FollowersInline(admin.TabularInline):
+    model = Follow
+    fk_name = 'following'
+    verbose_name_plural = 'Followers'
+    extra = 1
+
+# Inline class for following users
+class FollowingInline(admin.TabularInline):
+    model = Follow
+    fk_name = 'follower'
+    verbose_name_plural = 'Following'
+    extra = 1
 
 @register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = (
         "id",
         "username",
+        "get_followers_list",  # Add the custom method to list_display
+        "get_following_list",  # Add the custom method to list_display
         "is_public",
         "bio",
     )
     list_display_links = ("id", "username")
     search_fields = ("fisrt_name", "last_name", "id")
+    inlines = [FollowersInline, FollowingInline]
 
+    # Custom method to get the list of followers as a string
+    # Custom method to get the list of followers as a string
+    def get_followers_list(self, obj):
+        followers = obj.followers.all()
+        return ", ".join([follower.follower.username for follower in followers])
+
+    get_followers_list.short_description = "Followers"  # Set the column header
+
+    # Custom method to get the list of following users as a string
+    def get_following_list(self, obj):
+        following = obj.following.all()
+        return ", ".join([followed.following.username for followed in following])
+
+    get_following_list.short_description = "Following"  # Set the column header
 
 @admin.register(Follow)
 class FollowAdmin(admin.ModelAdmin):
