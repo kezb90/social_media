7be28bd1bd20c006d41a2993d163dc59dcdf19dc
kezb90 <92123322+kezb90@users.noreply.github.com>
2024-03-06 23:47:10 +0330

    add list editable

--- user_activity/admin.py
+++ user_activity/admin.py
@@ -3,8 +3,8 @@ from .models import Comment
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "post", "parent_comment", "content", "created_at")
+    list_display = ("id", "user", "post", "parent_comment", "content", "is_active", "created_at")
     search_fields = ("user__username", "post__title", "content")
-
+    list_editable = ["is_active"]
 
 admin.site.register(Comment, CommentAdmin)
