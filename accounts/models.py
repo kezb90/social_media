a8fd205fd421232dce51bfe121d12a2da0f70d5d
kezb90 <m90.rahmati@gmail.com>
2024-03-06 12:32:20 +0330

    add new method and FollowRequest model

--- accounts/models.py
+++ accounts/models.py
@@ -29,6 +29,39 @@ class Profile(User):
     def __str__(self):
         return self.username
 
+    """
+    This method uses the follow_requests_sent reverse relation
+    to filter profiles that have sent follow requests to the
+    current profile (self). The distinct() method ensures that
+    each sender profile is included only once in the result.
+    """
+
+    def follow_request_senders(self):
+        return Profile.objects.filter(
+            follow_requests_sent__to_user=self, follow_requests_sent__accepted=False
+        ).distinct()
+
+    def has_follow_requests(self):
+        return FollowRequest.objects.filter(to_user=self, accepted=False).exists()
+
+    """
+    The followers method uses the reverse relation followings
+    to filter profiles that are following the current profile (self).
+    """
+
+    # Get followers
+    def followers(self):
+        return Profile.objects.filter(followings__following=self)
+
+    """
+    The followings method uses the reverse relation followers
+    to filter profiles that the current profile is following.
+    """
+
+    # Get followings
+    def followings(self):
+        return Profile.objects.filter(followers__follower=self)
+
 
 class Follow(models.Model):
     follower = models.ForeignKey(
@@ -44,3 +77,19 @@ class Follow(models.Model):
 
     def __str__(self):
         return f"{self.follower.username} is follower of {self.following.username}"
+
+
+class FollowRequest(models.Model):
+    from_user = models.ForeignKey(
+        Profile, related_name="follow_requests_sent", on_delete=models.CASCADE
+    )
+    to_user = models.ForeignKey(
+        Profile, related_name="follow_requests_received", on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ["from_user", "to_user"]
+
+    def __str__(self):
+        return f"{self.from_user.username} wants to follow {self.to_user.username}"
